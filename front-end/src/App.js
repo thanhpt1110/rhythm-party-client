bd1f9b8f9434adc7b53862ca529a7c5fb8a5ab2e
DungLe2983 <lequocdung2983@gmail.com>
2023-10-31 23:00:26 +0700

    update front-end files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'remixicon/fonts/remixicon.css';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <div className='container w-full flex flex-row '>
+                <div className='w-[70%] bg-indigo-400'>Left Main</div>
+                <div className='w-[30%] bg-green-200'>Right Content</div>
+            </div>
+            <div className='audio bg-black text-white'>Audio</div>
+            <footer className=' bg-slate-500'>About us</footer>
+        </div>
+    );
 }
 
 export default App;
---
+++ front-end/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-red-200'>Header</div>
+  )
+}
+
+export default Header
