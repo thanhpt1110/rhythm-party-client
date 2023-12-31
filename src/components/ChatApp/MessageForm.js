66d6149adbddfb233bdf2288ad03205b4f17faa1
DungLe2983 <lequocdung2983@gmail.com>
2024-01-01 00:57:23 +0700

    fix css Chatroom

--- src/components/ChatApp/MessageForm.js
+++ src/components/ChatApp/MessageForm.js
@@ -29,7 +29,8 @@ const MessageForm = ({idRoom, onAddMessage}) => {
   return (
     <div className="bg-[#181818] fixed bottom-20 w-[70%] py-6 shadow-lg ">
       <form onSubmit={handleSendMessage} className="containerWrap px-2 flex flex-row h-10 ">
-          <input value={message} onChange={ e => setMessage(e.target.value)} className=" focus:outline-none bg-[#2B3440]  w-full px-4 rounded" type="text" />
+          <input value={message} onChange={ e => setMessage(e.target.value)}
+          className=" focus:outline-none bg-[#2B3440] w-2/3 px-4 rounded" type="text" />
           <button type="submit" className=" bg-gray-500 text-white rounded-r px-5 text-sm font-semibold">Send</button>
       </form>
     </div>
--- src/pages/RoomDetails.js
+++ src/pages/RoomDetails.js
@@ -249,7 +249,7 @@ useEffect(() => {
                               pauseOnHover
                               theme="dark" />
       <div className="w-[70%] bg-black text-white">
-        <div className='flex items-center text-3xl font-bold py-8 w-full gap-80'>
+        <div className='flex items-center text-3xl font-bold py-8 w-full gap-96'>
            <i
             className="ri-arrow-left-s-line cursor-pointer text-2xl text-white rounded-full bg-slate-700 px-3 py-2 hover:bg-slate-600 ml-8"
             onClick={handleBackClick}
@@ -258,9 +258,17 @@ useEffect(() => {
             <p className='text-xl'>{room && room.roomName}'s Room</p>
             <p className='text-xs text-gray-400'>ID: {room && room._id}</p>
           </div>
+        </div>
+        <div className='flex flex-col gap-3 ml-28 '>
+          <p className='text-xl font-bold text-gray-400 '>Chat Box</p>
+          <div className='flex gap-2 items-center'>
+
+            <p className='flex justify-center items-center w-5 h-5 rounded-full bg-green-500 text-xs font-bold '>24</p>
+             <p className='text-xs font-bold'>Online</p>
+          </div>
 
         </div>
-        <p className='text-xl font-bold text-gray-200 ml-32 mb-2'>Chat Box</p>
+
         <div className='max-h-[50%] overflow-y-auto '>
           <ChatBox messages= {listMessage}/>
           <MessageForm idRoom={room._id} onAddMessage = {callBackAddMessage}/>
